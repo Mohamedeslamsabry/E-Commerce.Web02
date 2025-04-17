a5b668f2273287e9dd0ec1587f9a1c1d3bc0e4d2
Mohamedeslamsabry <mohamed20eslam2004@gmail.com>
2025-04-17 13:17:31 +0200

    Part 07 Service Manager

---
+++ Core/Service_Abstrction/Product/IServiceManger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service_Abstrction.Product
+{
+    public interface IServiceManger
+    {
+        public IProductService productService { get; }
+    }
+}
---
+++ Core/Service_Implemention/Products/ServiceManger.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Domain_Layer.Contract;
+using Service_Abstrction.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service_Implemention.Products
+{
+    public class ServiceManger(IUnitOfWork _unitOfWork, IMapper _mapper) : IServiceManger
+    {
+        private readonly Lazy<IProductService> _LazyproductService = new Lazy<IProductService>(() => new ProductService(_unitOfWork, _mapper));
+        public IProductService productService => _LazyproductService.Value;
+    }
+}
