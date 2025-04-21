f7029d5a0b16dd3e221de1ecd9d288ab9e09285a
Mohamedeslamsabry <mohamed20eslam2004@gmail.com>
2025-04-21 17:26:02 +0200

    Solu Proplem In Sesion 03

--- Core/Service_Implemention/Products/ProductService.cs
+++ Core/Service_Implemention/Products/ProductService.cs
@@ -19,11 +19,18 @@ namespace Service_Implemention.Products
         #region GetAllProductsAsync
         public async Task<PaginatedResult<ProductDto>> GetAllProductsAsync(ProductQueryParamter productQuery)
         {
+            #region Paggention
+            var spec = new ProductCountSpecification(productQuery);
+            var TotalCount = await _unitOfWork.genricRepository<Product, int>().CountAsync(spec);
+            if (productQuery.pageSize == 0)
+            {
+                productQuery.pageSize = TotalCount;
+            }
+            #endregion
+
             var Specification = new ProductWithPrandAndTypeSpecification(productQuery);
             var Products = await _unitOfWork.genricRepository<Product, int>().GetAllAsync(Specification);
             var ProductsDto = _mapper.Map<IEnumerable<Product>, IEnumerable<ProductDto>>(Products);
-            var spec = new ProductCountSpecification(productQuery);
-            var TotalCount = await _unitOfWork.genricRepository<Product, int>().CountAsync(spec);
             return new PaginatedResult<ProductDto>(TotalCount, Products.Count(), productQuery.PageIndex, ProductsDto);
         }
         #endregion
