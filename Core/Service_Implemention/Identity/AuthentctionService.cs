9add4df991b51f32d49d9e48fe203e133feb3e5e
Mohamedeslamsabry <mohamed20eslam2004@gmail.com>
2025-05-01 19:20:57 +0300

    Part 07 JWT & Token Creation

--- Core/Service_Implemention/Identity/AuthentctionService.cs
+++ Core/Service_Implemention/Identity/AuthentctionService.cs
@@ -1,12 +1,17 @@
 ﻿using Domain_Layer.Exceptions;
 using Domain_Layer.Models.Identity;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using Service_Abstrction.Product;
 using Shared.DTO.Identity;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace Service_Implemention.Identity
 {
-    class AuthentctionService(UserManager<ApplicationUser> _userManager) : IAuthenticationService
+    class AuthentctionService(UserManager<ApplicationUser> _userManager , IConfiguration _configuration) : IAuthenticationService
     {
         public async Task<UserDTO> LoginAsync(LoginDTO loginDTO)
         {
@@ -20,7 +25,7 @@ namespace Service_Implemention.Identity
                 {
                     DisplayName = User.DisplayName,
                     Email = User.Email!,
-                    Token = GenerateTokenAsync(User)
+                    Token = await GenerateTokenAsync(User)
                 };
             }
             else
@@ -48,7 +53,7 @@ namespace Service_Implemention.Identity
                 {
                     DisplayName = User.DisplayName,
                     Email = User.Email!,
-                    Token = GenerateTokenAsync(User)
+                    Token = await GenerateTokenAsync(User)
                 };
             }
 
@@ -59,9 +64,36 @@ namespace Service_Implemention.Identity
             }
         }
 
-        private static string GenerateTokenAsync(ApplicationUser user)
+        private async Task<string> GenerateTokenAsync(ApplicationUser user)
         {
-            return "TODO";
+            #region Payload (Claim)
+            var Clamis = new List<Claim>()
+            {
+                new (ClaimTypes.Email, user.Email!),
+                new (ClaimTypes.Name, user.UserName!),
+                new (ClaimTypes.NameIdentifier, user.Id),
+            };
+
+            var Roles = await _userManager.GetRolesAsync(user);
+            foreach (var role in Roles)
+                Clamis.Add(new Claim(ClaimTypes.Role, role));
+            #endregion
+
+            var SecritKey = _configuration.GetSection("JWTOptions")["SecritKey"];
+            var Key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(SecritKey!));
+
+            var Credentials = new SigningCredentials(Key, SecurityAlgorithms.HmacSha256);
+
+            var Token = new JwtSecurityToken
+                            (
+                               issuer: _configuration["SecritKey:issuer"], //Claim
+                               audience: _configuration["SecritKey:audience"],//Claim
+                               expires: DateTime.Now.AddHours(1),//Claim
+                               claims: Clamis,//Claim
+                               signingCredentials: Credentials
+                            );
+            var returnToken = new JwtSecurityTokenHandler().WriteToken(Token);
+            return returnToken;
         }
     }
 }
--- Core/Service_Implemention/Products/ServiceManger.cs
+++ Core/Service_Implemention/Products/ServiceManger.cs
@@ -2,22 +2,18 @@
 using Domain_Layer.Contract;
 using Domain_Layer.Models.Identity;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
 using Service_Abstrction.Product;
 using Service_Implemention.Basket;
 using Service_Implemention.Identity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Service_Implemention.Products
 {
-    public class ServiceManger(IUnitOfWork _unitOfWork, UserManager<ApplicationUser> _userManager,IMapper _mapper, IBasketReposatiry _basketReposatiry) : IServiceManger
+    public class ServiceManger(IUnitOfWork _unitOfWork, UserManager<ApplicationUser> _userManager,IMapper _mapper, IBasketReposatiry _basketReposatiry , IConfiguration _configuration) : IServiceManger
     {
         private readonly Lazy<IProductService> _LazyproductService = new Lazy<IProductService>(() => new ProductService(_unitOfWork, _mapper));
         private readonly Lazy<IBasketService> _LazyBasketService = new Lazy<IBasketService>(() => new BasketService(_basketReposatiry, _mapper));
-        private readonly Lazy<IAuthenticationService> _authenticationService = new Lazy<IAuthenticationService>(() => new AuthentctionService(_userManager));
+        private readonly Lazy<IAuthenticationService> _authenticationService = new Lazy<IAuthenticationService>(() => new AuthentctionService(_userManager, _configuration));
         public IProductService productService => _LazyproductService.Value;
         public IBasketService BasketService => _LazyBasketService.Value;
         public IAuthenticationService authenticationService => _authenticationService.Value;
