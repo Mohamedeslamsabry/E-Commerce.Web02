27ef8370b5301bfd8dd8caa13d37e190d43d5e47
Mohamedeslamsabry <mohamed20eslam2004@gmail.com>
2025-05-05 20:19:31 +0300

    Part 02 Order Module Entities

---
+++ Core/Domain_Layer/Models/OrderModule/Order.cs
@@ -0,0 +1,22 @@
+﻿namespace Domain_Layer.Models.OrderModule
+{
+    public class Order : BaseEntity<Guid>
+    {
+        public string UserEmail { get; set; } = null!;
+        public DateTimeOffset OrderDate { get; set; } = DateTimeOffset.Now;
+        public OrderAddress Address { get; set; } = null!;
+        public OrderState State { get; set; } = OrderState.Pending;
+
+        #region DeliveryMethod And Fk
+        public DeliveryMethod DeliveryMethod { get; set; } = null!;
+        public int DeliveryMethodId { get; set; } // Fk 
+        #endregion
+
+        public ICollection<OrderItems> Items { get; set; } = [];
+        public decimal SubTotal { get; set; }
+        //[NotMapped]
+        //public decimal Total { get => SubTotal + DeliveryMethod.Price; }
+        public decimal GetTotal() => SubTotal + DeliveryMethod.Price;
+
+    }
+}
---
+++ Core/Domain_Layer/Models/OrderModule/ProductItemOrdered.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain_Layer.Models.OrderModule
+{
+    public class ProductItemOrdered
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = null!;
+        public string PictureUrl { get; set; } = null!;
+    }
+}
