f541d0e442fa214e19584126366ccd54b65c3fd7
Mohamedeslamsabry <mohamed20eslam2004@gmail.com>
2025-05-06 04:15:20 +0300

    Part 08 Order Controller [ Get All Delivery Methods - Get All Orders - Get Order By Id ]

--- Inferastructure/Presentation/ApiController/OrderController.cs
+++ Inferastructure/Presentation/ApiController/OrderController.cs
@@ -8,6 +8,7 @@ namespace Presentation.ApiController
 {
     public class OrderController(IServiceManger _serviceManger) : ApiBaseController
     {
+        #region CreateOrder
         [Authorize]
         [HttpPost]
         public async Task<ActionResult<OrderToReturnDTO>> CreateOrder(OrderDTO orderDTO)
@@ -16,5 +17,35 @@ namespace Presentation.ApiController
             var Result = await _serviceManger.orderService.CreateOrderAsync(orderDTO, GetEmailFromToken());
             return Ok(Result);
         }
+        #endregion
+
+        #region GetDelivaryMethodAsync
+        [HttpGet("DelivaryMethods")]
+        public async Task<ActionResult<DelivaryMethodDTO>> GetDelivaryMethods()
+        {
+            var Result = await _serviceManger.orderService.GetDelivaryMethodAsync();
+            return Ok(Result);
+        }
+        #endregion
+
+        #region AllOrderAsync
+        [Authorize]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<OrderToReturnDTO>>> AllOrderAsync()
+        {
+            var Result = await _serviceManger.orderService.AllOrderAsync(GetEmailFromToken());
+            return Ok(Result);
+        }
+        #endregion
+
+        #region GetOrderByIdAsync
+        [Authorize]
+        [HttpGet("{id}")]
+        public async Task<ActionResult<OrderToReturnDTO>> GetOrderByIdAsync(Guid id)
+        {
+            var Result = await _serviceManger.orderService.GetOrderByIdAsync(id);
+            return Ok(Result);
+        }
+        #endregion
     }
 }
