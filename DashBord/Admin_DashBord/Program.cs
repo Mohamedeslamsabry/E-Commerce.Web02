796b3aa971c0e7fa4e86b95e344fc00ec801388f
Mohamedeslamsabry <mohamed20eslam2004@gmail.com>
2025-05-09 17:34:25 +0300

    01 Discover & Setup Project

---
+++ DashBord/Admin_DashBord/Program.cs
@@ -0,0 +1,68 @@
+﻿using Domain_Layer.Models.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Persistence.Data.DbContexts;
+using Persistence.Data.Identity;
+
+namespace Admin_DashBord
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            #region Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            #region AddDbContext
+            builder.Services.AddDbContext<StroreDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DeafultConnection"));
+               
+            });
+            #endregion
+
+            #region IdentytDContext
+            builder.Services.AddDbContext<StoreIdentityDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("IdentityConnection"));
+            });
+            #endregion 
+
+            #region ApplicationUser And IdentityRole
+            builder.Services.AddIdentityCore<ApplicationUser>() // Options ممكن ابعت
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<StoreIdentityDbContext>();
+            #endregion
+
+            #endregion
+
+            var app = builder.Build();
+
+            #region Configure the HTTP request pipeline.
+           
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}"); 
+            #endregion
+
+            app.Run();
+        }
+    }
+}
