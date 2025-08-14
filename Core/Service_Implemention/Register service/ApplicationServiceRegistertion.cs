2dd9e034ca5db1623faacfc3ea8667c26941f747
Mohamedeslamsabry <mohamed20eslam2004@gmail.com>
2025-08-15 00:56:15 +0300

    Part 02 Service Manager With Factory Delegate

--- Core/Service_Implemention/Register service/ApplicationServiceRegistertion.cs
+++ Core/Service_Implemention/Register service/ApplicationServiceRegistertion.cs
@@ -1,12 +1,17 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.DependencyInjection;
 using Service_Abstrction.Product;
 using Service_Implemention.Basket;
+using Service_Implemention.Identity;
+using Service_Implemention.Orders;
 using Service_Implemention.Products;
+using Service_Implemention.Service_Implment;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IAuthenticationService = Service_Abstrction.Product.IAuthenticationService;
 
 namespace Service_Implemention.Register_service
 {
@@ -16,8 +21,36 @@ namespace Service_Implemention.Register_service
         public static IServiceCollection AddApplictionService(this IServiceCollection Services)
         {
             Services.AddAutoMapper(typeof(AssemblyRef).Assembly);
-            Services.AddScoped<IServiceManger, ServiceManger>();
+            Services.AddScoped<IServiceManger, ServiceMangerWithFactory>();
             //Services.AddScoped<IBasketService, BasketService>(); // Becouse ServiceManger
+
+            Services.AddScoped<IProductService, ProductService>();
+            Services.AddScoped<Func<IProductService>>(Provider =>
+            () => Provider.GetRequiredService<IProductService>()
+            );
+
+            Services.AddScoped<IBasketService, BasketService>();
+            Services.AddScoped<Func<IBasketService>>(Provider =>
+            () => Provider.GetRequiredService<IBasketService>()
+            );
+
+
+            Services.AddScoped<IOrderService, OrderService>();
+            Services.AddScoped<Func<IOrderService>>(Provider =>
+            () => Provider.GetRequiredService<IOrderService>()
+            );
+
+
+            Services.AddScoped<IAuthenticationService, AuthentctionService>();
+            Services.AddScoped<Func<IAuthenticationService>>(Provider =>
+            () => Provider.GetRequiredService<IAuthenticationService>()
+            );
+
+
+
+
+
+
             return Services;
         }
      
---
+++ Core/Service_Implemention/Service Implment/ServiceMangerWithFactory.cs
@@ -0,0 +1,23 @@
+﻿using Service_Abstrction.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service_Implemention.Service_Implment
+{
+    class ServiceMangerWithFactory (Func<IProductService> ProductFactory,
+        Func<IOrderService> OrderFactory ,
+        Func<IAuthenticationService> authFactory,
+        Func<IBasketService> BasketFactory) : IServiceManger
+    {
+        public IProductService productService => ProductFactory.Invoke();
+
+        public IBasketService BasketService => BasketFactory.Invoke();
+
+        public IAuthenticationService authenticationService => authFactory.Invoke();
+
+        public IOrderService orderService => OrderFactory.Invoke();
+    }
+}
